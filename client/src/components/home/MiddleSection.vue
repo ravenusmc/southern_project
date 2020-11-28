d3a348a8049e5f563660416e8f5dc9df1fedbd5d
ravenusmc <mcuddy77@gmail.com>
2020-11-28 15:10:45 -0500

    added responsive elements to home page

--- client/src/components/home/MiddleSection.vue
+++ client/src/components/home/MiddleSection.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <section>
-      <div class="div-one common-div-features">One</div>
+      <div class="div-one common-div-features"></div>
 
       <div class="div-two common-div-features">The Study</div>
 
-      <div class="div-three common-div-features">three</div>
+      <div class="div-three common-div-features"></div>
     </section>
   </div>
 </template>
@@ -18,26 +18,43 @@ export default {
 
 <style scoped>
 section {
-  border: 2px solid red;
   height: 600px;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
+  margin-bottom: 50px;
 }
 
 .common-div-features {
-	margin: 0 3% 0 3%;
-  height: 50%;
+  margin: 0 3% 0 3%;
+  height: 80%;
   width: 50%;
 }
 
 .div-one {
-  border: 2px solid blue;
+  background-image: url("../../assets/images/black_white.jpg");
+  background-size: 100% 100%;
+  background-position: center;
+  background-repeat: no-repeat;
 }
 
 .div-two {
-  border: 2px solid blue;
-	border-radius: 100%;
+  height: 10%;
+  width: 10%;
+  border-radius: 100%;
+}
+
+.div-three {
+  background-image: url("../../assets/images/lit_pic.jpg");
+  background-size: 100% 100%;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+
+@media only all and (max-width: 900px) {
+  section {
+    flex-direction: column;
+  }
 }
 </style>
