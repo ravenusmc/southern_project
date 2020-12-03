b71fa0f4eaa65a19d383d0a5f5fbd2d1e46f63d8
ravenusmc <mcuddy77@gmail.com>
2020-12-03 07:58:56 -0500

    started to add some code for the sentiment overtime graph

--- server/data.py
+++ server/data.py
@@ -13,11 +13,19 @@ class Analysis():
 	def __init__(self):
 		self.main_csv = pd.read_csv("./data/toc.csv") 
 
-	def practice(self):
-		# data = pd.read_csv("./data/toc.csv") 
-		# print(data.head())
+	#def test_code(self):
 		# print(self.main_csv.head())
+
+	def fetch_sentiment_overtime(self):
+		#Need to get the min and max years 
+		min_year = self.main_csv['Date'].min()
+		max_year = self.main_csv['Date'].min()
+		print(self.main_csv['Date'])
+		#Need to get all speeches by year 
+		#get sentiment of single speech during year
+		#average together all sentiments for speeches in given year
+		#put the year to the average sentiment for the graph data. 
 
 
 test = Analysis()
-test.practice()
+test.fetch_sentiment_overtime()
